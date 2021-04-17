7246e3e6eaa750940de723113088aa04b9e6d87b
408400355 <408400355@gms.tku.edu.tw>
2021-04-17 22:30:48 +0800

    0417

---
+++ 課後作業2/08.js
@@ -0,0 +1,49 @@
+function changeVideo(index) {
+    const player = document.querySelector("#player") ;
+    console.log('player' , player);
+    switch(index) {
+        case 1:
+            player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/RechrtUxfQc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            break;
+        case 2 :
+            player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/MRWX49Furew" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            break;
+        case 3:
+            player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/METhdbL_iMw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            break;
+        case 4:
+            player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/ZyDbq-lEKTo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            break;
+        case 5:
+            player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/NlsrJbVvjaA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            break;
+        case 6 :
+            player.innerHTML = `<iframe width="560" height="315" src="https://i.ytimg.com/vi/RechrtUxfQc/hqdefault.jpg"></iframe>`;
+            break;
+        case 7 :
+            player.innerHTML = `<iframe width="560" height="315" src="https://img.youtube.com/vi/MRWX49Furew/hqdefault.jpg"></iframe>`;
+            break;
+        case 8 :
+            player.innerHTML = `<iframe width="560" height="315" src="https://img.youtube.com/vi/METhdbL_iMw/hqdefault.jpg"></iframe>`;
+            break;
+        case 9 :
+            player.innerHTML = `<iframe width="560" height="315" src="https://img.youtube.com/vi/ZyDbq-lEKTo/hqdefault.jpg"></iframe>`;
+            break;
+        case 10 :
+            player.innerHTML = `<iframe width="560" height="315" src="https://img.youtube.com/vi/NlsrJbVvjaA/hqdefault.jpg"></iframe>`;
+            break;
+    }
+}
+
+function changeColor(index) {
+    const p = document.querySelector('#section');
+    switch(index) {
+        case 1: p.style.backgroundColor = 'blue';
+            break;
+        case 2 : p.style.backgroundColor = 'purple';
+            break;
+        case 3 : p.style.backgroundColor = 'pink';
+            break;
+    }
+   
+}
